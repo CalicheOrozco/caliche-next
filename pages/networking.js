ccdd3dc5d461b8bbba17ecc97eedc3fb8456e90f
CalicheOrozco <caliche2da1@gmail.com>
2022-04-28 15:08:37 -0500

    feature: networking game

---
+++ pages/networking.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ContainerBlock from "../components/ContainerBlock";
+import Networking from "../components/Networking";
+
+const networking = () => {
+  return (
+    <ContainerBlock title="Conocer - Caliche Orozco">
+      <Networking />
+    </ContainerBlock>
+  );
+};
+
+export default networking;
