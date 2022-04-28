4e14af8650fde5cc2a25ad8394f44889b85ce0c9
CalicheOrozco <caliche2da1@gmail.com>
2022-04-28 04:40:47 -0500

    feature: "nunca nunca game"

---
+++ components/NuncaNunca.js
@@ -0,0 +1,69 @@
+import React, { useState, useEffect } from "react";
+import myData from "../constants/data";
+
+export default function NuncaNunca() {
+  const [frases, setFrases] = useState([]);
+  const [frase, setFrase] = useState(null);
+
+  const getRandomInt = (min, max) => {
+    return Math.floor(Math.random() * (max - min)) + min;
+  };
+
+  const generateFrase = () => {
+    const num = getRandomInt(0, frases.length);
+    const frase = frases[num];
+    const newFrases = frases.splice(num, 1);
+    setFrase(frase);
+    console.log(newFrases);
+    console.log("numero de frases: ", frases.length);
+    return frase;
+  };
+
+  const listasFrases = useEffect(() => {
+    fetch("/yoNuncaNunca.json")
+      .then((res) => res.json())
+      .then((res) => setFrases(res));
+    generateFrase();
+  }, []);
+
+  console.log("test: " + frases.length);
+  return (
+    <section className="bg-white dark:bg-gray-800">
+      <div className="max-w-6xl mx-auto h-48 bg-white dark:bg-gray-800">
+        <h1 className=" text-5xl md:text-8xl xl:text-9xl font-bold py-20 text-center md:text-left">
+          Yo nunca, nunca
+        </h1>
+      </div>
+      <div className="bg-[#F1F1F1] -mt-10 dark:bg-gray-900 h-full">
+        <div className="text-center max-w-6xl mx-auto pt-20">
+          <p
+            className="leading-loose text-2xl md:text-4xl font-semibold mx-4 py-10 md:py-20"
+            style={{ lineHeight: "3rem" }}
+          >
+            {frase}
+          </p>
+          {frases.length > 0 ? (
+            <button
+              type="button"
+              onClick={() => {
+                generateFrase();
+              }}
+              className="text-white bg-green-500 hover:bg-green-700 focus:outline-none focus:ring-4 focus:ring-green-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:bg-green-500 dark:hover:bg-green-600 dark:focus:ring-green-800"
+            >
+              {frase === undefined ? "Generar frase" : "Generar otra frase"}
+            </button>
+          ) : (
+            <div role="alert">
+              <div class="bg-red-500 text-white font-bold rounded-t px-4 py-2">
+                ¡Se han terminado los Nunca nunca!
+              </div>
+              <div class="border border-t-0 border-red-400 rounded-b bg-red-100 px-4 py-3 text-red-700">
+                <p>Recuerda que me puedes contactar para agregar mas frases.</p>
+              </div>
+            </div>
+          )}
+        </div>
+      </div>
+    </section>
+  );
+}
---
+++ pages/nuncanunca.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ContainerBlock from "../components/ContainerBlock";
+import NuncaNunca from "../components/NuncaNunca";
+
+const nuncanunca = () => {
+  return (
+    <ContainerBlock>
+      <NuncaNunca />
+    </ContainerBlock>
+  );
+};
+
+export default nuncanunca;
