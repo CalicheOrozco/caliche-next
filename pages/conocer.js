818ede195caae098b9d1ec0f1494bb1c94ed7419
CalicheOrozco <caliche2da1@gmail.com>
2022-04-28 15:08:54 -0500

    feature: conocer game

---
+++ pages/conocer.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Conocer from "../components/Conocer";
+import ContainerBlock from "../components/ContainerBlock";
+
+const conocer = () => {
+  return (
+    <ContainerBlock title="Conocer - Caliche Orozco">
+      <Conocer />
+    </ContainerBlock>
+  );
+};
+
+export default conocer;
