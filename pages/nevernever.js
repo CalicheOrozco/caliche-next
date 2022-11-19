144f4e3ca6d0178e46da46097a97b6aa29fabc74
Caliche Orozco <caliche2da1@gmail.com>
2022-11-18 18:02:38 -0600

    future: nevernever site

---
+++ pages/nevernever.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import ContainerBlock from "../components/ContainerBlock";
+import NeverNever from '../components/NeverNever';
+
+function nevernever() {
+  return (
+    <ContainerBlock title="I Never Never - Caliche Orozco">
+      <NeverNever />
+    </ContainerBlock>
+  )
+}
+
+export default nevernever
