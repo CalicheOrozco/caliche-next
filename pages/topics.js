3c46c8d55d9aa028403e7ff6a901c31c3c1167ec
Caliche Orozco <caliche2da1@gmail.com>
2023-02-04 19:04:44 -0600

    future: topics section

---
+++ pages/topics.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import ContainerBlock from "../components/ContainerBlock";
+import Topics from '../components/Topics';
+
+function topics() {
+    return (
+        <ContainerBlock title="Topics - Caliche Orozco">
+          <Topics />
+        </ContainerBlock>
+      )
+}
+
+export default topics
