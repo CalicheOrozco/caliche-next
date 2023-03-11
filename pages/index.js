c1a1acab8202c049dbf9e94a1bb4ff9ab02698e9
Caliche Orozco <caliche2da1@gmail.com>
2023-03-11 00:58:30 -0600

    fix: responsive problems

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
 import ContainerBlock from "../components/ContainerBlock";
+import Footer from "../components/Footer";
 import Hero from "../components/Hero";
+import Navbar from "../components/Navbar";
 
 export default function Home() {
   return (
@@ -7,7 +9,15 @@ export default function Home() {
       title="Caliche Orozco | Front-End Developer | Landing Page"
       description="Study Engineering in Computer Systems, Promoter of technology and science events in order to solve social problems and improve the Latin American economy using technology as the main ally."
     >
-      <Hero />
+      <div className="min-h-screen flex flex-col">
+      <Navbar />
+      <div className="relative bg-cover bg-center  flex-grow flex items-center justify-center">
+        <Hero />
+      </div>
+      <Footer />
+      
+    </div>
+      
     </ContainerBlock>
   );
 }
