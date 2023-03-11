4201e2b75506acbaeb35ba0267aa934b610fa4fc
Caliche Orozco <caliche2da1@gmail.com>
2023-03-11 17:15:04 -0600

    updated: change svg for react icons

--- components/Projects.js
+++ components/Projects.js
@@ -1,6 +1,7 @@
 import React, { useContext } from "react";
 import myData from "../constants/data";
 import LanguageContext from "../context/lenguageContext";
+import { BsGithub } from "react-icons/bs";
 
 export default function Projects() {
   const { lenguage } = useContext(LanguageContext);
@@ -17,12 +18,13 @@ export default function Projects() {
       </div>
       {/* Grid starts here */}
       <div className="bg-[#F1F1F1] dark:bg-gray-900">
-        <div className="max-w-6xl mx-auto grid grid-cols-1 md:grid-cols-2 gap-8 py-2">
+        <div className="max-w-6xl mx-auto grid grid-cols-1 md:grid-cols-2 gap-8 p-4">
           {myData.projects.map((proj, idx) => (
             <ProjectCard
               title={proj.title}
               link={proj.link}
               imgUrl={proj.imgUrl}
+              github={proj.github} 
             />
           ))}
         </div>
@@ -31,21 +33,26 @@ export default function Projects() {
   );
 }
 
-const ProjectCard = ({ title, link, imgUrl }) => {
+const ProjectCard = ({ title, link, imgUrl, github }) => {
   return (
-    <a href={link} target="_blank" className="w-full block shadow-2xl">
-      <div className="relative overflow-hidden">
-        <div className="h-72 object-cover">
+    
+      <div className="relative overflow-hidden rounded-2xl">
+        <div className="object-cover">
+        <a href={link} target="_blank" className="w-full block shadow-2xl">
           <img
             src={imgUrl}
-            alt="portfolio"
+            alt={`${title} portfolio`}
             className="transform hover:scale-125 transition duration-2000 ease-out object-cover h-full w-full"
-          />
+          />          
+          </a>
         </div>
+        <a href={github} target="_blank" className="w-full block shadow-2xl">
         <h1 className="absolute top-10 left-10 text-gray-50 font-bold text-xl bg-green-600 rounded-md px-2">
-          {title}
-        </h1>
+        < BsGithub className="inline-block mx-2" />
+        {title}
+        </h1> 
+        </a>
       </div>
-    </a>
+    
   );
 };
